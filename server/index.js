8b92116689f6e297bd8f9c07074eafdd4b08f55f
Juan Avila <jmanuelavilaperez@gmail.com>
2024-10-18 07:59:02 -0500

    update chatbot

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
 import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Chatbot UCompensar</h1>');
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('chat message', (message) => {
+        console.log(`A user sent a message: ${message}`);
+        io.emit('chat message', message);
+    });
 });
 
-app.listen(port, () => {
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
