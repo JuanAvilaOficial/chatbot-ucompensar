5de6eda377dfe492c83d1889436d04bf3ad86b09
Juan Avila <jmanuelavilaperez@gmail.com>
2024-11-04 17:40:52 -0500

    update chatbot

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
+import { exec } from 'node:child_process';
+
 const port = process.env.PORT ?? 3000;
 
 
@@ -16,11 +18,12 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log('A user connected');    
+    
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 
-    socket.on('chat message', (message) => {
+    socket.on('chat message', (message) => {        
         io.emit('chat message', message);
     });
 });
